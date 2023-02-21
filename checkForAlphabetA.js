2aaa2972bfcd2f0b7b2b49b4437862338ecb8bd7
Abhishek Choudhary <81501764+chaudhary-abhishek@users.noreply.github.com>
2023-02-21 19:41:56 +0530

    included the solution without using includes

--- checkForAlphabetA.js
+++ checkForAlphabetA.js
@@ -4,10 +4,25 @@
 // 4. Name your variables differently.
 // 5. Stop using var from now on.
 
+// const checkForAlphabetA = (text)=>{
+//     if(text.toLowerCase().includes("a",0)) return `Includes a`;
+//     else return `Does not include a`;
+// }
+
+
+// console.log(checkForAlphabetA("Abhishek"));
+
+
 const checkForAlphabetA = (text)=>{
-    if(text.toLowerCase().includes("a",0)) return `Includes a`;
-    else return `Does not include a`;
+    let count = 0;
+    for(let i = 0;i<text.length;i++){
+        if(text[i]=='a'||text[i]=='A')
+         count++
+        
+    }
+    if(count>0)
+    return `Includes a`;
+    else
+    return `Does not includes a`;
 }
-
-
 console.log(checkForAlphabetA("Abhishek"));
